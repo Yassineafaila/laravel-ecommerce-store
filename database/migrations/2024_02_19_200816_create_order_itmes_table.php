e328373592a4277015ccf81a0974721ddfd0ce28
Yassineafaila <yassineafaila55@gmail.com>
2024-02-20 00:12:27 +0000

    add the migraiton for this models

---
+++ database/migrations/2024_02_19_200816_create_order_itmes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{co
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orderItems', function (Blueprint $table) {
+
+            $table->unsignedBigInteger("order_id");
+            $table->unsignedBigInteger("product_id");
+            $table->integer("quantity");
+            $table->float("subTotal");
+
+            $table->foreign("order_id")->references("id")->on("orders")->onDelete("cascade");
+            $table->foreign("product_id")->references("id")->on("products")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_itmes');
+    }
+};
